6e6f042c60d62b838fcc8407782ff6bd93f20ddf
Pandurang Chopade <pchopade@accoonaglobal.com>
2020-08-18 20:48:06 +0530

    first commit code

---
+++ TestApp/TestApp/TestApp/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnGet_Clicked(object sender, EventArgs e)
+        {
+            lblResult.Text = string.Empty;
+            lblResult.TextColor = Color.Black;
+            WebHelper webHelper = new WebHelper();
+            try
+            {
+                ProtobufModelDto protobufModelDto = await webHelper.GetProtoBufData();
+                if (!string.IsNullOrEmpty(protobufModelDto.Name))
+                {
+                    lblResult.Text = "Result = " + " Id : " + protobufModelDto.Id + ", Name : " + protobufModelDto.Name + ", StringValue : " + protobufModelDto.StringValue;
+                }
+                else
+                {
+                    lblResult.TextColor = Color.Red;
+                    lblResult.Text = "Sorry, something went wrong";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResult.TextColor = Color.Red;
+                lblResult.Text = ex.Message;
+            }
+
+        }
+
+        private async void BtnPost_Clicked(object sender, EventArgs e)
+        {
+            lblResult.Text = string.Empty;
+            lblResult.TextColor = Color.Black;
+            WebHelper webHelper = new WebHelper();
+            try
+            {
+                ProtobufModelDto protobufModelDto = await webHelper.PostProtoBufData();
+                if (!string.IsNullOrEmpty(protobufModelDto.Name))
+                {
+                    lblResult.Text = "Result = " + " Id : " + protobufModelDto.Id + ", Name : " + protobufModelDto.Name + ", StringValue : " + protobufModelDto.StringValue;
+                }
+                else
+                {
+                    lblResult.TextColor = Color.Red;
+                    lblResult.Text = "Sorry, something went wrong";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResult.TextColor = Color.Red;
+                lblResult.Text = ex.Message;
+            }
+
+        }
+    }
+}
---
+++ TestApp/TestApp/TestApp/WebHelper.cs
@@ -0,0 +1,92 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class WebHelper
+    {
+        public async Task<ProtobufModelDto> GetProtoBufData()
+        {
+            ProtobufModelDto protobufModelDto = new ProtobufModelDto();
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-protobuf"));
+
+                string sURL = "http://192.168.0.102:45455/api/values/4";
+                HttpResponseMessage httpResponse = await client.GetAsync(sURL);
+
+                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+
+                    var streamData = httpResponse.Content.ReadAsStreamAsync().Result;
+
+                    protobufModelDto = Serializer.Deserialize<ProtobufModelDto>(streamData);
+
+
+                    // Parse the response body. Blocking!
+                    //var p = httpResponse.Content.ReadAsAsync<ProtobufModelDto>(new[] { new ProtoBufFormatter() }).Result;
+                    //Console.WriteLine("{0}\t{1};\t{2}", p.Name, p.StringValue, p.Id);
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)httpResponse.StatusCode, httpResponse.ReasonPhrase);
+                }
+
+                return protobufModelDto;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public async Task<ProtobufModelDto> PostProtoBufData()
+        {
+            string sURL = "http://192.168.0.102:45455/api/values";
+            try
+            {
+                var client = new HttpClient();
+
+                var request = new HttpRequestMessage(HttpMethod.Post, sURL);
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-protobuf"));
+
+                var protobufModelDto = new ProtobufModelDto
+                {
+                    Id = 1234,
+                    Name = "ganesh gaikwad",
+                    StringValue = "reader3@abc.com"
+                };
+
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    ProtoBuf.Serializer.Serialize(stream, protobufModelDto);
+                    request.Content = new ByteArrayContent(stream.ToArray());
+
+                    var response = client.SendAsync(request).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var reader = ProtoBuf.Serializer.Deserialize<ProtobufModelDto>(await response.Content.ReadAsStreamAsync());
+
+                        return reader;
+                    }
+                }
+
+                return protobufModelDto;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
